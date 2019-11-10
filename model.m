c7126ebdc39025b38f222aaa3e661280b7959c29
deikare <bbgolawski@gmail.com>
2019-11-10 12:08:42 +0100

    Improve model.m
    Deleted line disp("...") - it was unnecessary.

--- model.m
+++ model.m
@@ -4,7 +4,7 @@ function [t, y] = model(tspan, y0, F1, Fd, p, q, Tp, Tsym)
     
     function dydt = dstate(t, y)
         dydt = zeros(2,1);
-        F1_act = interp1(0:Tp:Tsym, F1, t);
+        F1_act = interp1(0:Tp:Tsym, F1, t); %interpolacja dyskretnych danych dla chwili t
         Fd_act = interp1(0:Tp:Tsym, Fd, t);
         
         if (y(1) < 0)
@@ -15,9 +15,8 @@ function [t, y] = model(tspan, y0, F1, Fd, p, q, Tp, Tsym)
             y(2) = 0;
         end
         
-        dydt(1) = F1_act + Fd_act - p * nthroot(y(1), 4); %tutaj trzeba dodac opoznienie jakos
+        dydt(1) = F1_act + Fd_act - p * nthroot(y(1), 4); %juz zostalo dodane opoznienie
         dydt(2) = p * nthroot(y(1), 4) - q * nthroot(y(2), 6);
-        disp("ode");
     end
 end
 
