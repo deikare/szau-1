373b5945588ef168300d72667a6cada7710b5c9a
deikare <bbgolawski@gmail.com>
2019-11-10 13:16:29 +0100

    Fix H1.m, add static values in const.m
    Fixed model, now works well.

--- const.m
+++ const.m
@@ -6,18 +6,27 @@ al2 = 16;
 
 F1in = 78;
 Fd0 = 15;
-h2 = 33.7852;
+h1_0 = 0;
+h2_0 = 33.7852;
+V1_0 = V1(h1_0, C1); %warunki pocz¹tkowe objetosci
+V2_0 = V2(h2_0, C2); %warunki pocztkowe objetosci
 
-p = al1/sqrt(C1); %jak uzaleznilem wszystkie wyp³ywy zamiast od h1, h2, to V1, V2, to tu licze wspolczynniki
-q = al2/nthroot(C2, 3); %jak cos nthroot(C2, 3) to pierwiastek 3iego stopnia z C2
+p = al1/nthroot(C1, 4); %jak uzaleznilem wszystkie wyp³ywy zamiast od h1, h2, to V1, V2, to tu licze wspolczynniki
+q = al2/nthroot(C2, 6); %jak cos nthroot(C2, 3) to pierwiastek 3iego stopnia z C2
 Ch1 = 1 / sqrt(C1); %a te to pomocnicze przy liczeniu wysokosci z objetosci
 Ch2 = 1 / nthroot(C2, 3);
 
 Top = 90;
-Tsym = 200;
+Tsym = 5000;
 Tp = 2;
+
+V1ust = ((F1in+Fd0)/p)^4; %w stanie ustalonym
+V2ust = ((F1in+Fd0)/q)^6;
+
+h1ust = Ch1 * sqrt(V1ust); %w stanie ustalonym
+h2ust = Ch2 * nthroot(V2ust, 3);
 
 n = Tsym/Tp;
 
-y0 = [0, V2(h2, C2)];
+y0 = [V1_0, V2_0];
 
