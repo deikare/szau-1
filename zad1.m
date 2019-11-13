0572f833eeb5e9ba8cd868577516d3329fb94f57
deikare <bbgolawski@gmail.com>
2019-11-13 23:13:44 +0100

    Add better display of plots
    Now Matlab plots everything on one figure instead of separate ones.

--- zad1.m
+++ zad1.m
@@ -22,18 +22,60 @@ h2 = H2(V(:, 2), Ch2);
 h1_lin = H1(V_lin(:, 1), Ch1);
 h2_lin = H2(V_lin(:, 2), Ch2);
 
-figure;
+tiledlayout(2,2);
+
+nexttile;
 stairs(t, h1);
-% stairs(t, V(:, 1));
 hold on;
-% stairs(t_lin, V_lin(:, 1));
 stairs(t_lin, h1_lin);
+title('h1');
+ylabel('h1(t)');
+xlabel('t');
+legend('nieliniowy', 'zlinearyzowany');
 hold off;
 
-figure;
+nexttile;
 stairs(t, h2);
-% stairs(t, V(:, 2));
 hold on;
-% stairs(t_lin, V_lin(:, 2));
 stairs(t_lin, h2_lin);
+title('h2');
+ylabel('h2(t)');
+xlabel('t');
+legend('nieliniowy', 'zlinearyzowany');
 hold off;
+
+nexttile;
+stairs(t, V(:, 1));
+hold on;
+stairs(t_lin, V_lin(:, 1));
+title('V1');
+ylabel('V1(t)');
+xlabel('t');
+legend('nieliniowy', 'zlinearyzowany');
+hold off;
+
+nexttile;
+stairs(t, V(:, 2));
+hold on;
+stairs(t_lin, V_lin(:, 2));
+title('V2');
+ylabel('V2(t)');
+xlabel('t');
+legend('nieliniowy', 'zlinearyzowany');
+hold off;
+
+% figure;
+% stairs(t, h1);
+% % stairs(t, V(:, 1));
+% hold on;
+% % stairs(t_lin, V_lin(:, 1));
+% stairs(t_lin, h1_lin);
+% hold off;
+% 
+% figure;
+% stairs(t, h2);
+% % stairs(t, V(:, 2));
+% hold on;
+% % stairs(t_lin, V_lin(:, 2));
+% stairs(t_lin, h2_lin);
+% hold off;
